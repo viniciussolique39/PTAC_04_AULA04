fb2dce722523e18874d7f37ecd88d7c462384651
gabriela <113440728+viniciussolique39@users.noreply.github.com>
2023-08-15 16:07:55 -0400

    Criação das Funções

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,40 @@
 'use server'
 
-const getUserAuthenticated = (user) => {
+const Usuario=[
+    {
 
+    name: "Vinicius de Souza Solique",
+    email: "vinicius@gmail.com", 
+    password : "12345678910",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" 
+   },
+
+   {
+
+    name: "Victor de Souza Solique",
+    email: "victor@gmail.com", 
+    password : "1234",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" 
+   },
+
+   {
+
+    name: "Ingrid de Souza Solique",
+    email: "ingrid@gmail.com", 
+    password : "12345",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" 
+   }
+
+];
+
+const getUserAuthenticated = (user) => {
+ Usuario.map(user => user.name === name, user.email === email && user.password === password, user.token === token);
+ return  (name, email, password, token)
 }
 
 const getUsers = () =>{
-        
+        return (
+            name, email, password, token
+        )
 }
 export { getUsers, getUserAuthenticated };
